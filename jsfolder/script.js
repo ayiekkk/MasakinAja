d905d6b3fcdf0b15c5afb54cbd4a0c3a88c86d69
ayiekkk <mohamadalief547@gmail.com>
2025-02-17 09:46:34 +0700

    fix

--- jsfolder/script.js
+++ jsfolder/script.js
@@ -7,11 +7,3 @@ function toggleMenu() {
   
   hamburgerIcon.classList.toggle('active');
 }
-<<<<<<< HEAD:script.js
-
-function search() {
-  const query = document.getElementById('search').value;
-  alert('Mencari: ' + query);
-}
-=======
->>>>>>> eb4024c80aa5810f9f8d19820f8c24b1a14d2ddb:jsfolder/script.js
